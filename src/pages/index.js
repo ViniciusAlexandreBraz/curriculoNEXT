e2103871073547144fce184218896407a2894d55
Vinicius <Viniskilne@gmail.com>
2023-08-08 22:19:39 -0400

    primeiro commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Menu from '@/components/menu'
+import Cabecalho from '@/components/cabecalho'
+
+export default function Home() {
+  return (
+    <>
+      <Menu/>
+      <Cabecalho/>
+    </>
+  )
+}
